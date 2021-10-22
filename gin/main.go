0cba5c7d4233b4008138d8a449f3449adc2e6085
张雄 <1493943799@qq.com>
2021-10-22 23:32:17 +0800

    remove JWT token

--- gin/main.go
+++ gin/main.go
@@ -3,6 +3,8 @@ package main
 import(
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
+	"path"
 )
 
 func init(){
@@ -10,26 +12,41 @@ func init(){
 }
 
 func main(){
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context){
-		c.JSON(200, gin.H{"message":"pong"})
-	})
+	r := gin.New()
+	//r.GET("/ping", func(c *gin.Context){
+	//	c.JSON(200, gin.H{"message":"pong"})
+	//})
 
 
-	r.GET("/test", func(c *gin.Context){routeGetTest(c)})
-	r.POST("/test", func(c *gin.Context){routePostTest(c)})
+	r.GET("/user/:name", routeUser)
+	r.POST("/test", routePostTest)
 	r.Run(":8080")
 }
 
-func routeGetTest(c *gin.Context){
+func routeUser(c *gin.Context){
 	fmt.Println("test")
 	name := c.Param("name")
 	pw := c.Query("pw")
 	nick := c.PostForm("nick")
 
+	path1, path2, path3 := "/aaa/", "bbb", "cc"
+	log.Printf("%s\n", path.Join(path1, path2, path3))
 
 	fmt.Println(name, pw, nick)
-	c.JSON(200, gin.H{"message":"test"})
+	c.JSON(200, gin.H{"message":name})
+}
+
+func routeAge(c *gin.Context){
+	fmt.Println("test")
+	age := c.Param("age")
+	pw := c.Query("pw")
+	nick := c.PostForm("nick")
+
+	path1, path2, path3 := "/aaa/", "bbb", "cc"
+	log.Printf("%s\n", path.Join(path1, path2, path3))
+
+	fmt.Println(pw, nick)
+	c.JSON(200, gin.H{"message":age})
 }
 
 func routePostTest(c *gin.Context){
