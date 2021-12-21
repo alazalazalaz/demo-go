f028932b59dcfc2de2bdf5b37c1542c2e6301e65
zhangxiong <1493943799@qq.com>
2021-12-21 20:35:05 +0800

    up

--- base/random.go
+++ base/random.go
@@ -1,9 +1,9 @@
-package main 
+package main
 
-import(
+import (
 	"fmt"
+	"math/rand"
 	"time"
-	ra "math/rand"
 )
 
 
@@ -13,10 +13,24 @@ func main(){
 	fmt.Println(Random(0, 1))//结果只有0
 	fmt.Println(Random(0, 2))//结果为0或者1
 
+	//随机字符串
+	randStr()
 }
 
 //@return 左闭右开，结果会包含min，不会有max
 func Random(min, max int) int {
-	ra.Seed(time.Now().Unix())
-	return ra.Intn(max-min) + min
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+func randStr(){
+	n := 10
+	keyLetter := []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	keyBytes := make([]byte, n)
+	for i := range keyBytes {
+		keyBytes[i] = keyLetter[rand.Intn(len(keyLetter))]
+	}
+	newClientKey := string(keyBytes)
+
+	fmt.Println(newClientKey)
 }
