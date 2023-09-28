a11db9229815efe0c617f2b1acac4b6f282f2691
zhangxiong <zhangxiong@tap4fun.com>
2023-09-28 16:05:36 +0800

    fix

--- mychatgpt/consts/openai.go
+++ mychatgpt/consts/openai.go
@@ -2,9 +2,9 @@ package consts
 
 const (
 	//OPENAI_API_KEY = ""//我的
-	OPENAI_API_KEY = "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"
-	OPENAI_ORG_ID  = "org-OKh6KhOXdctrSfNfplQWM4xR"
-	OPENAI_API_V1  = "https://api.openai.com/v1/"
+	//OPENAI_API_KEY = "sk-qw8hUlhnVkdO**uIAhWFwblZab0cmqk"
+	OPENAI_ORG_ID = "org-OKh6KhOXdctrSfNfplQWM4xR"
+	OPENAI_API_V1 = "https://api.openai.com/v1/"
 
 	OPENAI_MODEL_MODELS      = "models"
 	OPENAI_MODEL_CHAT        = "chat/completions"
