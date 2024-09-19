fed9069a955ea666ea81834ef69ff93680dfbe87
zhangxiong <zhangxiong@tap4fun.com>
2024-09-19 15:47:14 +0800

    update

--- base/test.go
+++ base/test.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"encoding/hex"
 	"fmt"
 )
 
 func main() {
-	s, err := hex.DecodeString("xx")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(s)
+	fmt.Println("hello world")
 }
