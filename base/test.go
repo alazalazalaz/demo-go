3def0e9aafdb4b9c6adce48d38b27db0a4b6665a
zhangxiong <zhangxiong@tap4fun.com>
2024-09-19 17:08:06 +0800

    up

--- base/test.go
+++ base/test.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello world")
+	resp, err := http.Get("http://www.baidu.com")
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println("请求失败:", err)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("读取响应体失败:", err)
+		return
+	}
+
+	fmt.Println(string(body))
 }
